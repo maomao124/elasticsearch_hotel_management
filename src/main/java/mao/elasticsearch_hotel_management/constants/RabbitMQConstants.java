f145f1c5c868e31314fe44985eb1ec89a41ca5df
mao <1296193245@qq.com>
2022-06-04 14:29:20 +0800

    elasticsearch_hotel_management

--- src/main/java/mao/elasticsearch_hotel_management/constants/RabbitMQConstants.java
+++ src/main/java/mao/elasticsearch_hotel_management/constants/RabbitMQConstants.java
@@ -15,7 +15,7 @@ package mao.elasticsearch_hotel_management.constants;
 
 public class RabbitMQConstants
 {
-    public static final String EXCHANGE_NAME = "hotel.topic.exchange";
+    public static final String EXCHANGE_NAME = "hotel.exchange";
     public static final String INSERT_QUEUE_NAME = "hotel.insert.queue";
     public static final String DELETE_QUEUE_NAME = "hotel.delete.queue";
     public static final String INSERT_KEY = "hotel.insert";
