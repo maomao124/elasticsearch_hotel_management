8b5bb4fa48decb88a4eb74bf7f52bfd0f2ceb956
mao <1296193245@qq.com>
2022-06-04 13:44:17 +0800

    elasticsearch_hotel_management

---
+++ src/main/java/mao/elasticsearch_hotel_management/ElasticsearchHotelManagementApplication.java
@@ -0,0 +1,15 @@
+package mao.elasticsearch_hotel_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchHotelManagementApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ElasticsearchHotelManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/elasticsearch_hotel_management/ElasticsearchHotelManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.elasticsearch_hotel_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchHotelManagementApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
